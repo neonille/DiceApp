149f3350bbc7dda20a6885b67c806592a28618e3
Niille <neo_nille@hotmail.com>
2022-05-08 16:13:26 +0200

    Top level await bug
    fixed

--- src/main.js
+++ src/main.js
@@ -5,10 +5,8 @@ import './index.css'
 
 let app = createApp(App)
 
-let x = await fetch("https://6277979d08221c96846731ce.mockapi.io/api/v1/code");
-x = await x.json();
-console.log(x)
-app.provide("code", x)
+
+app.provide("code", import.meta.env.CODE)
 
 
 
