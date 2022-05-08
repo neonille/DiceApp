e17c694da48202b2f6d26b6f03c5a2b853053aaf
Niille <neo_nille@hotmail.com>
2022-05-08 16:08:34 +0200

    Tailwind
    Installed tailwind

---
+++ src/components/Login.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="flex flex-col  bg-white p-10 rounded-md shadow-md">
+        <h1 class="text-4xl text-center pb-7">Login</h1>
+        <label for="uname">username</label>
+        <input class="shadow-inner p-1 rounded-sm border-2" type=" text" name="uname" id="uname">
+        <label for="pass">password</label>
+        <input class="shadow-inner p-1 rounded-sm border-2" type="text" name="pass" id="pass">
+        <div class="flex flex-row place-content-center gap-10 pt-6">
+            <button class="bg-red-300 py-2 px-5">asd</button>
+            <button class="bg-blue-300 py-2 px-5">asd</button>
+        </div>
+
+    </div>
+</template>
+<style>
+</style> 
+<script setup>
+import { inject, nextTick, onMounted } from 'vue';
+
+
+onMounted(() => {
+    let code = inject("code")
+    console.log(code)
+})
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './index.css'
 
-createApp(App).mount('#app')
+
+let app = createApp(App)
+
+let x = await fetch("https://6277979d08221c96846731ce.mockapi.io/api/v1/code");
+x = await x.json();
+console.log(x)
+app.provide("code", x)
+
+
+
+app.mount('#app')
